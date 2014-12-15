449c6f4d2c4c579a31db9673ea7571d628522312
Matthew Ahlberg <matthewahlberg@matthews-air.home>
2014-12-14 23:46:34 -0800

    heroku

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+
 
 module Calendar
   class Application < Rails::Application
